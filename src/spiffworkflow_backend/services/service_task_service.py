260610c58669ee0a2d662bf163730bd099b3d4e8
Jon Herron <jon.herron@yahoo.com>
2022-08-30 11:39:00 -0400

    Some TODOs from the chat

--- src/spiffworkflow_backend/services/service_task_service.py
+++ src/spiffworkflow_backend/services/service_task_service.py
@@ -40,6 +40,7 @@ class ServiceTaskService:
         try:
             import airflow.providers
             from airflow.models import BaseOperator
+            # TODO filter only operators with no subclasses
             yield from _classes_of_type_in_pkg(airflow.providers, type(BaseOperator))
         except:
             pass
@@ -49,6 +50,9 @@ class ServiceTaskService:
         init_sig = inspect.signature(operator_class.__init__)
         params_to_skip = ['self', 'kwargs']
         params = filter(lambda param: param.name not in params_to_skip, init_sig.parameters.values())
+        # TODO add new attrib to tell if type is optional
+        # TODO remove iterable, take inner type
+        # TODO on union form set of types
         params = [{"name": param.name, "type": str(param.annotation) } for param in params]
         return params
 
