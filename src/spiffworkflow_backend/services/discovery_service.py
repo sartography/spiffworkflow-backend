02f12b9557ef02b14f33b1d4d4d3484fac9e5835
Jon Herron <jon.herron@yahoo.com>
2022-08-30 13:54:41 -0400

    Factor out DiscoveryService, add typing

---
+++ src/spiffworkflow_backend/services/discovery_service.py
@@ -0,0 +1,51 @@
+"""Discovery_service."""
+import importlib
+import inspect
+import pkgutil
+import types
+from typing import Any, Generator
+
+DiscoveredClass = Any
+DiscoveredModule = Any
+DiscoveredClassGenerator = Generator[tuple[str, DiscoveredClass], None, None]
+DiscoveredModuleGenerator = Generator[tuple[str, DiscoveredModule], None, None]
+
+class DiscoveryService:
+    """Discovers classes and modules within a given package."""
+    
+    @staticmethod
+    def modules_in_pkg(pkg) -> DiscoveredModuleGenerator:
+        """Recursively yields a (name, module) for each module in the given pkg."""
+
+        for finder, name, ispkg in pkgutil.iter_modules(pkg.__path__):
+            if ispkg:
+                # TODO couldn't get this to work with exec_module
+                sub_pkg = finder.find_module(name).load_module(name)
+                yield from DiscoveryService.modules_in_pkg(sub_pkg)
+                continue
+            try:
+                spec = finder.find_spec(name)
+                module = types.ModuleType(spec.name)
+                spec.loader.exec_module(module)
+                yield name, module
+            except:
+                pass
+
+    @staticmethod
+    def classes_in_pkg(pkg) -> DiscoveredClassGenerator:
+        """Recursively yields a (name, class) for each class in each module in the given pkg."""
+
+        for module_name, module in DiscoveryService.modules_in_pkg(pkg):
+            for clz_name, clz in inspect.getmembers(module, inspect.isclass):
+                if clz.__module__ == module_name:
+                    yield clz_name, clz
+
+    @staticmethod
+    def classes_of_type_in_pkg(pkg, clz_type) -> DiscoveredClassGenerator:
+        """Recursively yields a (name, class) for each class in each module in the given pkg 
+        that isinstance of the given clz_type."""
+
+        for clz_name, clz in DiscoveryService.classes_in_pkg(pkg):
+            if isinstance(clz, clz_type):
+                yield clz_name, clz
+
--- src/spiffworkflow_backend/services/service_task_service.py
+++ src/spiffworkflow_backend/services/service_task_service.py
@@ -3,80 +3,73 @@ import importlib
 import inspect
 import pkgutil
 import sys
-import types
+from typing import Any, Generator, TypedDict
 
-# TODO these could be moved to a more generic location
-def _modules_in_pkg(pkg):
-    for finder, name, ispkg in pkgutil.iter_modules(pkg.__path__):
-        if ispkg:
-            # TODO couldn't get this to work with exec_module
-            sub_pkg = finder.find_module(name).load_module(name)
-            yield from _modules_in_pkg(sub_pkg)
-            continue
-        try:
-            spec = finder.find_spec(name)
-            module = types.ModuleType(spec.name)
-            spec.loader.exec_module(module)
-            yield name, module
-        except:
-            pass
+from spiffworkflow_backend.services.discovery_service import DiscoveryService
 
-def _classes_in_pkg(pkg):
-    for module_name, module in _modules_in_pkg(pkg):
-        for clz_name, clz in inspect.getmembers(module, inspect.isclass):
-            if clz.__module__ == module_name:
-                yield clz_name, clz
+class OperatorParameter(TypedDict):
+    name: str
+    type: str
+    required: bool
 
-def _classes_of_type_in_pkg(pkg, clz_type):
-    for clz_name, clz in _classes_in_pkg(pkg):
-        if isinstance(clz, clz_type):
-            yield clz_name, clz
+class Operator(TypedDict):
+    name: str
+    parameters: list[OperatorParameter]
 
+OperatorClass = Any
+OperatorClassGenerator = Generator[tuple[str, OperatorClass], None, None]
 
 class ServiceTaskService:
 
     @staticmethod
-    def _available_airflow_operator_classes():
-        try:
-            import airflow.providers
-            from airflow.models import BaseOperator
-            # TODO filter only operators with no subclasses
-            yield from _classes_of_type_in_pkg(airflow.providers, type(BaseOperator))
-        except:
-            pass
+    def _available_airflow_operator_classes() -> OperatorClassGenerator:
+        """Yields name and class for all airflow operators that are available for use in 
+        service tasks."""
+
+        #try:
+        import airflow.providers
+        from airflow.models import BaseOperator
+        # TODO filter only operators with no subclasses
+        yield from DiscoveryService.classes_of_type_in_pkg(airflow.providers, type(BaseOperator))
+        #except:
+        #    pass
 
     @staticmethod
-    def _infer_operator_params(operator_class):
+    def _parse_operator_params(operator_class) -> list[OperatorParameter]:
+        """Parses the init of the given operator_class to build a list of OperatorParameters."""
+
         init_sig = inspect.signature(operator_class.__init__)
         params_to_skip = ['self', 'kwargs']
         params = filter(lambda param: param.name not in params_to_skip, init_sig.parameters.values())
-        # TODO add new attrib to tell if type is optional
         # TODO remove iterable, take inner type
         # TODO on union form set of types
-        params = [{"name": param.name, "type": str(param.annotation) } for param in params]
+        params = [{"name": param.name, "type": str(param.annotation), "required": True } for param in params]
         return params
 
     @classmethod
-    def available_operator_classes(cls):
+    def available_operator_classes(cls) -> OperatorClassGenerator:
+        """Yields name and class for all operators that are available for use in a service task."""
+
         # TODO maybe premature to have a place to aggregate other operator types?
         yield from cls._available_airflow_operator_classes()
 
-    # TODO wtf is the syntax here?
-    #type OperatorParm = Dict[k in [name, parameter], str]
-
     @classmethod
-    def available_operators(cls):
-        # TODO do we define models to add types?
+    def available_operators(cls) -> list[Operator]:
+        """Returns a list of all operator names and init parameters that are available for use in 
+        a service task."""
+
         available_operators = [{
-            "name": name, 
-            "parameters": cls._infer_operator_params(clz)
-        } for name, clz in cls.available_operator_classes()]
+            "name": operator_name, 
+            "parameters": cls._parse_operator_params(operator_class)
+        } for operator_name, operator_class in cls.available_operator_classes()]
 
         return list(available_operators)
 
     @classmethod
-    def scripting_additions(cls):
-        # TODO add types
+    def scripting_additions(cls) -> dict[str, OperatorClass]:
+        """Returns a dictionary of operator names and classes for loading into a 
+        scripting engine instance."""
+
         operator_classes = list(cls.available_operator_classes())
         scripting_additions = {name: clz for name, clz in operator_classes}
         return scripting_additions
