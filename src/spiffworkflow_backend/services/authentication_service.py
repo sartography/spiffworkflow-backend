4c093cb23f0ea92ed3908ced388dc370ce95892c
Jon Herron <jon.herron@yahoo.com>
2022-09-06 15:56:15 -0400

    Back out airflow dependency and related changes

--- src/spiffworkflow_backend/services/authentication_service.py
+++ src/spiffworkflow_backend/services/authentication_service.py
@@ -132,13 +132,7 @@ class PublicAuthenticationService:
             open_id_client_secret_key,
         ) = get_open_id_args()
         try:
-            decoded_token = jwt.decode(
-                id_token,
-                options={
-                    "verify_signature": False,
-                    "verify_aud": False,
-                },
-            )
+            decoded_token = jwt.decode(id_token, options={"verify_signature": False})
         except Exception as e:
             raise ApiError(
                 code="bad_id_token", message="Cannot decode id_token", status_code=401
--- tests/spiffworkflow_backend/integration/test_process_api.py
+++ tests/spiffworkflow_backend/integration/test_process_api.py
@@ -13,6 +13,7 @@ from flask_bpmn.models.db import db
 from tests.spiffworkflow_backend.helpers.base_test import BaseTest
 from tests.spiffworkflow_backend.helpers.test_data import load_test_spec
 from tests.spiffworkflow_backend.helpers.test_data import logged_in_headers
+from werkzeug.test import TestResponse
 
 from spiffworkflow_backend.exceptions.process_entity_not_found_error import (
     ProcessEntityNotFoundError,
@@ -1297,7 +1298,7 @@ class TestProcessApi(BaseTest):
         test_process_group_id: str,
         test_process_model_id: str,
         headers: Dict[str, str],
-    ) -> any:
+    ) -> TestResponse:
         """Create_process_instance."""
         load_test_spec(test_process_model_id, process_group_id=test_process_group_id)
         response = client.post(
@@ -1318,7 +1319,7 @@ class TestProcessApi(BaseTest):
         exception_notification_addresses: Optional[list] = None,
         primary_process_id: Optional[str] = None,
         primary_file_name: Optional[str] = None,
-    ) -> any:
+    ) -> TestResponse:
         """Create_process_model."""
         process_model_service = ProcessModelService()
 
