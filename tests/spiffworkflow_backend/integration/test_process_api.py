e557a9dd1e4b4c63b363c82911005e0acf788f67
jasquat <jasquat@users.noreply.github.com>
2022-10-11 10:03:16 -0400

    fixed db error message checks

--- tests/spiffworkflow_backend/integration/test_process_api.py
+++ tests/spiffworkflow_backend/integration/test_process_api.py
@@ -1373,7 +1373,7 @@ class TestProcessApi(BaseTest):
         api_error = json.loads(response.get_data(as_text=True))
         assert api_error["code"] == "task_error"
         assert (
-            'Activity_CauseError: TypeError:can only concatenate str (not "int") to str'
+            'TypeError:can only concatenate str (not "int") to str'
             in api_error["message"]
         )
 
@@ -1454,8 +1454,7 @@ class TestProcessApi(BaseTest):
             message = outbox[0]
             assert message.subject == "Unexpected error in app"
             assert (
-                message.body
-                == 'Activity_CauseError: TypeError:can only concatenate str (not "int") to str'
+                'TypeError:can only concatenate str (not "int") to str' in message.body
             )
             assert message.recipients == process_model.exception_notification_addresses
 
