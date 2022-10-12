0b21a5d4c5ad9564bf96c505f9087b904b95d743
burnettk <burnettk@users.noreply.github.com>
2022-10-12 13:09:13 -0400

    refactor bin/save_all_bpmn.py into service code

---
+++ src/spiffworkflow_backend/services/data_setup_service.py
@@ -0,0 +1,82 @@
+"""Data_setup_service."""
+from spiffworkflow_backend.services.process_model_service import ProcessModelService
+from spiffworkflow_backend.services.spec_file_service import SpecFileService
+
+
+class DataSetupService:
+    """DataSetupService."""
+
+    @staticmethod
+    def save_all() -> list:
+        """Save_all."""
+        failing_process_models = []
+        process_models = ProcessModelService().get_process_models()
+        for process_model in process_models:
+            if process_model.primary_file_name:
+
+                bpmn_xml_file_contents = SpecFileService.get_data(
+                    process_model, process_model.primary_file_name
+                )
+                bad_files = [
+                    "B.1.0.bpmn",
+                    "C.1.0.bpmn",
+                    "C.2.0.bpmn",
+                    "C.6.0.bpmn",
+                    "TC-5.1.bpmn",
+                ]
+                if process_model.primary_file_name in bad_files:
+                    continue
+                print(f"primary_file_name: {process_model.primary_file_name}")
+                try:
+                    SpecFileService.update_file(
+                        process_model,
+                        process_model.primary_file_name,
+                        bpmn_xml_file_contents,
+                    )
+                except Exception as ex:
+                    failing_process_models.append(
+                        (
+                            f"{process_model.process_group_id}/{process_model.id}/{process_model.primary_file_name}",
+                            str(ex),
+                        )
+                    )
+                # files = SpecFileService.get_files(
+                #     process_model, extension_filter="bpmn"
+                # )
+                # bpmn_etree_element: EtreeElement = (
+                #     SpecFileService.get_etree_element_from_binary_data(
+                #         bpmn_xml_file_contents, process_model.primary_file_name
+                #     )
+                # )
+                # if len(files) == 1:
+                # try:
+                #     new_bpmn_process_identifier = (
+                #         SpecFileService.get_bpmn_process_identifier(
+                #             bpmn_etree_element
+                #         )
+                #     )
+                #     if (
+                #         process_model.primary_process_id
+                #         != new_bpmn_process_identifier
+                #     ):
+                #         print(
+                #             "primary_process_id: ", process_model.primary_process_id
+                #         )
+                #         # attributes_to_update = {
+                #         #     "primary_process_id": new_bpmn_process_identifier
+                #         # }
+                #         # ProcessModelService().update_spec(
+                #         #     process_model, attributes_to_update
+                #         # )
+                # # except Exception as exception:
+                # except Exception:
+                #     print(f"BAD ONE: {process_model.id}")
+                #     # raise exception
+            else:
+                failing_process_models.append(
+                    (
+                        f"{process_model.process_group_id}/{process_model.id}",
+                        "primary_file_name not set",
+                    )
+                )
+        return failing_process_models
